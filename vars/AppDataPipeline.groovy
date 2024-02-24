2d9d4f553582ebb7cae0c756390c19b04555d3da
jcescobarn <jcescobarn@unal.edu.co>
2024-02-23 20:54:02 -0500

    AppDataPipeline

--- vars/AppDataPipeline.groovy
+++ vars/AppDataPipeline.groovy
@@ -1,44 +1,21 @@
-import SonarScanner
+pipeline {
+    agent any
 
-/**
- * Clase para definir un pipeline de Jenkins para escanear un proyecto de aplicación.
- * Este pipeline utiliza SonarScanner para realizar el análisis estático del código.
- * @jcescobarn 
- * @version 1.0
- */
-
-class AppDataPipeline {
-
-    /** Configuración del pipeline */
-    def pipelineConfig
-
-    /**
-     * Constructor de la clase AppDataPipeline.
-     * @param pipelineConfig Configuración del pipeline.
-     */
-    AppDataPipeline(Map pipelineConfig) {
-        this.pipelineConfig = pipelineConfig
+    parameters {
+        string(name: 'projectKey', defaultValue: 'defaultKey', description: 'Key of the project')
+        string(name: 'projectName', defaultValue: 'defaultName', description: 'Name of the project')
+        string(name: 'sourcePath', defaultValue: '.', description: 'Source path of the project')
     }
 
-    /**
-     * Método para construir y ejecutar el pipeline.
-     * Este método define las etapas y pasos del pipeline.
-     */
-    public void pipeline() {
-        pipeline {
-            agent any
-
-            stages {
-                stage('Scan Project') {
-                    steps {
-                        script {
-                            SonnarScanner.scanApplication(
-                                projectKey: pipelineConfig.projectKey,
-                                projectName: pipelineConfig.projectName,
-                                sourcePath: pipelineConfig.sourcePath
-                            )
-                        }
-                    }
+    stages {
+        stage('Scan Project') {
+            steps {
+                script {
+                    SonnarScanner.scanApplication(
+                        projectKey: params.projectKey,
+                        projectName: params.projectName,
+                        sourcePath: params.sourcePath
+                    )
                 }
             }
         }
